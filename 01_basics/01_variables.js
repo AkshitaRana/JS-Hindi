759cedc57069727162a4b2f32155c331e201c7e5
Akshita Rana <68386442+AkshitaRana@users.noreply.github.com>
2024-02-27 07:18:25 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1234
+var name = "Akshita"
+let email = "a@gmail.com"
+city = "Bengaluru"
+
+
+console.log(accountId);
+console.table([accountId, name, email, city])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all code as newer version of javascript.
+let name = "Akshita"
+let age = 72
+let isLoggedIn = true
+
+console.log(typeof name);
+console.log(typeof 7)
