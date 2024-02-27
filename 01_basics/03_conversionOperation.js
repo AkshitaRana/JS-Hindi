2e3b1776a8981585c6795efc604c4cae2193d530
Akshita Rana <68386442+AkshitaRana@users.noreply.github.com>
2024-02-27 08:10:19 +0000

    conversionOpeartions done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = "33"
+
+let convertScore = Number (score)
+// console.log(convertScore);
+// console.log(typeof convertScore)
+
+// 33aa = Nan
+// true = 1; false = 0
+
+
+let isBoolean = 1
+let convertIsBoolean = Boolean(isBoolean)
+// console.log(convertIsBoolean)
+// console.log(typeof convertIsBoolean)
+
+//1=true; 0=false
+//"" = false
+//"Akshita" = true
+
+let value = 33
+// console.log(typeof value)
+let convertString = String(value) 
+// console.log(convertString)
+// console.log(typeof convertString)
+
+// **************************OPERATIONS*************************
+
+let data = 3
+let negData = -data
+// console.log(negData);
+
+let str1 ="Hello"
+let str2 =" Akshita"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log(+true);
+// console.log(+"");
+
+let num = 100
+num++;
+console.log(num);
