a7154ff0f6a7ca91c2352118517361bec2682c57
Akshita Rana <68386442+AkshitaRana@users.noreply.github.com>
2024-02-28 08:01:50 +0000

    dates done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,37 @@
+let myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate); //object
+
+// let newDate = new Date(2024, 1, 28) //Months start from 0-11 in js
+// console.log(newDate.toDateString());
+
+// let newDate = new Date(2024, 1, 28, 13, 2)
+// console.log(newDate.toLocaleString());
+
+// let newDate = new Date("2024-02-28")
+// console.log(newDate.toLocaleString());
+
+let newDate = new Date("02-28-2024")
+// console.log(newDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(newDate.getTime());
+
+
+// console.log(Math.floor(Date.now()/1000)); // to find current seconds from the day of declaration
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+// console.log(newDate.getTime());
+
+// newDate.toLocaleString('default', { 
+//     weekday: "long"
+// }) We can customize TolocaleString more in depth
