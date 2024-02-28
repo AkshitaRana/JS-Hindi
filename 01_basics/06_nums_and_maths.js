1a8ba0db62f5dbb2b85f834e092e2fb4d8901548
Akshita Rana <68386442+AkshitaRana@users.noreply.github.com>
2024-02-28 07:12:12 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+// ********************NUMBER*********************
+// const score = 400
+// console.log(score);
+
+// const scr = new Number(100)
+// console.log(scr);
+
+// console.log(scr.toString().length);
+// console.log(scr.toFixed(2));
+
+// const newNumber = 123.6567
+// console.log(newNumber.toPrecision(3));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ****************************MATHS*****************************
+// console.log(Math);
+// console.log(Math.abs(-34));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,5,7,8,10));
+// console.log(Math.max(2,5,7,8,10));
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
