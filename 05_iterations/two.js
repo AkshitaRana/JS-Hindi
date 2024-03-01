b539b8c6ebf55235645ed977e929eaf6088e0eca
Akshita Rana <68386442+AkshitaRana@users.noreply.github.com>
2024-03-01 17:33:45 +0000

    while and do while loop done

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 0
+// while (index<11) {
+//     console.log(`Value: ${index}`);
+//     index = index+2 
+// }
+
+const myArray = ["Orange", "Mango", "Grapes"]
+let arr = 0
+while(arr<myArray.length){
+    // console.log(`Fruit name is: ${myArray[arr]}`);
+    arr = arr+1
+}
+
+
+let score = 11
+do {
+    console.log(`value is: ${score}`);
+    score++
+} while (score<=10);
